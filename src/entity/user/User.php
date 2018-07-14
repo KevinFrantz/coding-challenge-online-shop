40e1624d9d8540a748644cbf0ffa4e1861f99a42
Kevin Frantz <mail@kevin-frantz.de>
2018-07-14 10:59:51 +0200

    Defined email as unique identifier

--- src/entity/user/User.php
+++ src/entity/user/User.php
@@ -14,6 +14,7 @@ final class User implements UserInterface
     private $name;
     
     /**
+     * Unique identifier in database
      * @var string
      */
     private $email;
@@ -52,9 +53,5 @@ final class User implements UserInterface
     {
         return $this->email;
     }
-
-    public function getId(): int
-    {}
-
 }
 
