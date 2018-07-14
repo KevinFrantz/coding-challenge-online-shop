8217753e3bd226e4cecf208027b9ca7f01c81047
Kevin Frantz <mail@kevin-frantz.de>
2018-07-14 10:58:29 +0200

    Implemented user

--- src/entity/user/User.php
+++ src/entity/user/User.php
@@ -8,23 +8,50 @@ namespace entity\user;
  */
 final class User implements UserInterface
 {
+    /**
+     * @var string
+     */
+    private $name;
+    
+    /**
+     * @var string
+     */
+    private $email;
+    
+    /**
+     * @var string
+     */
+    private $passwordHash;
+    
     public function setName(string $name): void
-    {}
+    {
+        $this->name = $name;
+    }
 
     public function getName(): string
-    {}
+    {
+        return $this->name;
+    }
 
     public function setEmail(string $email): void
-    {}
+    {
+        $this->email = $email;
+    }
 
     public function getPasswordHash(): string
-    {}
+    {
+        return $this->passwordHash;
+    }
 
     public function setPasswordHash(string $hash): void
-    {}
+    {
+        $this->passwordHash = $hash;
+    }
 
     public function getEmail(): string
-    {}
+    {
+        return $this->email;
+    }
 
     public function getId(): int
     {}
